686c156428f02c55b0ae4f71ee0382d0313585a7
MikuMikuyukis <35860554+MikuMikuyukis@users.noreply.github.com>
2019-07-01 19:35:26 +0800

    serverchan

---
+++ serverchanbootNo0.0.3.py
@@ -0,0 +1,17 @@
+﻿# coding= utf-8
+import requests,urllib.request,socket,re,time
+url = "yourURL"
+title = "设备已经启动"
+#常量定义
+time.sleep(10)
+getip=urllib.request.urlopen('http://ip.42.pl/raw')
+getip2=getip.read()
+string_ip = str(getip2)
+#获取ip地址
+ip = str(re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b",string_ip))
+#格式化ip地址
+pcname = socket.gethostname()
+dataout = str("主机名:"+pcname+"设备公网ip地址:"+ip)
+data={"text":title,"desp":dataout}
+requests.post(url,data=data)
+#请求ServerchanAPI
